b6061447ba8721c53bb1361e779fb7d9b5e37910
arun <1462918312@qq.com>
2020-07-22 13:54:44 +0800

    删除不用的代码

--- third-integration/zentao/src/main/java/nl/sri/zentao/controller/OpenClassController.java
+++ third-integration/zentao/src/main/java/nl/sri/zentao/controller/OpenClassController.java
@@ -2,8 +2,6 @@ package nl.sri.zentao.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import nl.sri.zentao.entity.ZtProject;
 import nl.sri.zentao.entity.ZtTask;
 import nl.sri.zentao.mapper.ZtProjectMapper;
 import nl.sri.zentao.mapper.ZtTaskMapper;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
